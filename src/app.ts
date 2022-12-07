c14385fee826f3f8d64cecf390e37a6bf5b35839
Dmytro Knyshuk <contact.dimial@gmail.com>
2022-12-07 18:19:14 +0200

    Added first lesson 02.02

---
+++ src/app.ts
@@ -0,0 +1,65 @@
+showHello('greeting', 'TypeScript');
+
+function showHello(divName: string, name: string) {
+    const elt = document.getElementById(divName);
+    elt.innerText = `Hello from ${name}`;
+}
+
+//===================================
+//Task 02.01 + 02.02
+
+enum Category { HTML, CSS, JavaScript, Angular };
+
+type Book = {
+    id: number,
+    title: string,
+    author: string,
+    available: boolean,
+    category: Category,
+};
+
+function getAllBooks(): readonly Book[] {
+    const books = <const>[
+        { id: 1, title: 'Refactoring JavaScript', category: Category.JavaScript, author: 'Evan Burchard', available: true },
+        { id: 2, title: 'JavaScript Testing', category: Category.JavaScript, author: 'Liang Yuxian Eugene', available: false },
+        { id: 3, title: 'CSS Secrets', category: Category.CSS, author: 'Lea Verou', available: true },
+        { id: 4, title: 'Mastering JavaScript Object-Oriented Programming', category: Category.JavaScript, author: 'Andrea Chiarelli', available: true }
+    ];
+    return books;
+
+}
+
+function logFirstAvailable(books: readonly Book[]): void {
+    console.log(`Number of books in the library ${books.length}`);
+    const title = books.find(({ available }) => available)?.title;
+    console.log(`First available book: ${title}`);
+}
+
+function getBookTitlesByCategory(inputCategory: Category): string[] {
+    const books = getAllBooks();
+    return books
+        .filter(book => book.category === inputCategory)
+        .map(book => book.title);
+}
+
+function logBookTitles(titles: Array<string>): void {
+    titles.forEach(title => console.log(title));
+}
+
+function getBookAuthorByIndex(index: number): [title: string, author: string] {
+    const books = getAllBooks();
+    const { title, author } = books[index];
+    return [title, author];
+}
+
+function calcTotalPages(): void {
+    const data = <const>[
+        { lib: 'libName1', books: 1_000_000_000, avgPagesPerBook: 250 },
+        { lib: 'libName2', books: 5_000_000_000, avgPagesPerBook: 300 },
+        { lib: 'libName3', books: 3_000_000_000, avgPagesPerBook: 280 }
+    ];
+    const r = data.reduce((acc: bigint, obj) => {
+        return acc + BigInt(obj.books) * BigInt(obj.avgPagesPerBook);
+    }, 0n);
+    console.log(r);
+}
